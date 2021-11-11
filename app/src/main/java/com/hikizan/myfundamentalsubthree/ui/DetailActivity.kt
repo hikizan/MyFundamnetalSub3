7d13e147d145ab7fae45d8e2423bf63d37af70f3
Akhmad Fauzan Prayogi <akhmadfauzanprayogi@gmail.com>
2021-11-11 13:34:08 +0700

    need debug!

--- app/src/main/java/com/hikizan/myfundamentalsubthree/ui/DetailActivity.kt
+++ app/src/main/java/com/hikizan/myfundamentalsubthree/ui/DetailActivity.kt
@@ -68,6 +68,7 @@ class DetailActivity : AppCompatActivity() {
                         }
                     })
                 }
+
                 isFavorite = false
                 setFabFav(false)
 
